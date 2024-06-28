36f5917ab5f49eb093fb2d93878adc6cf2e5bb38
ManuelWestermeier <westermeier111@gmail.com>
2024-06-28 17:14:07 +0200

    update

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-import { createServer } from "wsnet-server"
+import { createServer } from "wsnet-server";
 
-const counter = 0
-const clients = []
+let counter = 0;
+let clients = [];
 
 createServer({ port: 8080 }, async client => {
     
-    clients.push(client)
+    clients.push(client);
 
-    client.say("set-counter", ++counter)
+    client.say("set-counter", ++counter);
 
     client.onSay("increase-counter", () => {
-        counter++
-        clients.forEach(client => client.say("set-counter", counter));
-    })
+        counter++;
+        client.say("set-counter", counter);
+        // Create a copy of the clients array for safe iteration
+        [...clients].forEach(_client => _client.say("set-counter", counter));
+    });
 
     client.onclose = () => {
-        clients = clients.filter(c => c != client)
-    }
+        clients = clients.filter(c => c !== client);
+    };
 
-})
+});
